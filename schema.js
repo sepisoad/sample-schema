3fde009229559e0e35877c7363bb3a2a7d446ab5
Sepehr Aryani <sepehr.aryani@gmail.com>
2019-05-19 10:58:24 +0430

    added real schema

--- schema.js
+++ schema.js
@@ -1,9 +1,52 @@
-const schema_a = {
-  "name": "lol",
-  "age": 2500,
-  "gender": "male"
+const userLight = {
+  "type": "array",
+  "items": {
+    "$ref": "#/definitions/UsersLightElement"
+  },
+  "definitions": {
+    "UsersLightElement": {
+      "type": "object",
+      "additionalProperties": false,
+      "properties": {
+        "firstName": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "firstName"
+      ],
+      "title": "UsersLightElement"
+    }
+  }
+}
+
+const userExtended = {
+  "type": "array",
+  "items": {
+    "$ref": "#/definitions/UsersExtendedElement"
+  },
+  "definitions": {
+    "UsersExtendedElement": {
+      "type": "object",
+      "additionalProperties": false,
+      "properties": {
+        "firstName": {
+          "type": "string"
+        },
+        "lastName": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "firstName",
+        "lastName"
+      ],
+      "title": "UsersExtendedElement"
+    }
+  }
 }
 
 module.exports = {
-  a: schema_a
+  userLight,
+  userExtended
 }
